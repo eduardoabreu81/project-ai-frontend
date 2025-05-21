eb86459fa4ab6bb241aad07fd7486b5b84a6b99f
eduardoabreu81 <eduardoabreu81@gmail.com>
2025-05-21 19:00:03 -0300

    Add files via upload

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
+
 import React from 'react';
+
 export default function Login() {
-  return <h1>Página de Login funcionando</h1>;
+  return <div style={{ padding: '2rem' }}><h2>Login funcionando!</h2></div>;
 }
